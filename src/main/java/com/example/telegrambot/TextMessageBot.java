5977e587af323a80fa49943637c5c3a9207fc6bd
Lysak <ldv@ldv.com.ua>
2021-06-01 14:58:54 +0300

    final

--- src/main/java/com/example/telegrambot/TextMessageBot.java
+++ src/main/java/com/example/telegrambot/TextMessageBot.java
@@ -25,57 +25,70 @@ public class TextMessageBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            handleMessageUpdate( update );
+            handleMessageUpdate(update);
         }
+
         if (update.hasCallbackQuery()) {
-            handleCallbackQuery( update );
+            handleCallbackQuery(update);
         }
-        ;
     }
 
     private void handleMessageUpdate(Update update) {
         Long chatId = update.getMessage().getChatId();
         String messageText = update.getMessage().getText();
 
-        sendText( chatId, "Вы написали: " + messageText + true);
+        sendText(chatId, "Вы написали: " + messageText, true);
     }
 
-    private void sendText(Long chatId, String s) {
-    }
-
-    private void sendText(Long chatId, String text, boolean appendKeyboard) {
-        //create a object that contains the information to send back the message
+    private void sendText(long chatId, String text, boolean appendKeyboard) {
         SendMessage sendMessageRequest = new SendMessage();
-        sendMessageRequest.setChatId( Long.toString( chatId ) ); //who should get the message? the sender from which we got the message...
-        sendMessageRequest.setText( text );
-        sendMessageRequest.setReplyMarkup( createdUpLowKeyboard() );
+        sendMessageRequest.setChatId(Long.toString(chatId));
+        sendMessageRequest.setText(text);
+
+        if (appendKeyboard) {
+            sendMessageRequest.setReplyMarkup(createUpLowKeyboard());
+        }
+
         try {
-            sendApiMethod( sendMessageRequest );
+            sendApiMethod(sendMessageRequest);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
-    private ReplyKeyboard createdUpLowKeyboard() {
+    private ReplyKeyboard createUpLowKeyboard() {
         InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
-
 
         keyboard.setKeyboard(
                 Collections.singletonList(
-                        Arrays.asList( InlineKeyboardButton.builder().text( "low" ).callbackData( "make low" ).build(),
-                                InlineKeyboardButton.builder().text( "up" ).callbackData( "make up" ).build()
+                        Arrays.asList(
+                                InlineKeyboardButton.builder().text("low").callbackData("make_low").build(),
+                                InlineKeyboardButton.builder().text("UP").callbackData("make_up").build()
                         )
                 )
         );
+
         return keyboard;
     }
 
     private void handleCallbackQuery(Update update) {
         String callbackQuery = update.getCallbackQuery().getData();
-        String text = update.getCallbackQuery().getMessage().getText();
-        Long chatId = update.getCallbackQuery().getFrom().getId();
-        sendText( chatId, "Вы нажали на кнопку " + callbackQuery + "текст под кнопкой " + text );
 
+        String text = update.getCallbackQuery().getMessage().getText();
+        text = extractUserInput(text);
+
+        text = switch (callbackQuery) {
+            case "make_low" -> text.toLowerCase();
+            case "make_up" -> text.toUpperCase();
+            default -> text;
+        };
+
+        Long chatId = update.getCallbackQuery().getFrom().getId();
+
+        sendText(chatId, text,false);
     }
 
+    private String extractUserInput(String rawText) {
+        return rawText.substring(13);
+    }
 }
